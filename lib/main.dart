f35731583960730bea9110748cbf6745d1f85698
dzvoud <dzvoud@gmail.com>
2022-11-01 09:51:12 +0100

    add button

--- lib/main.dart
+++ lib/main.dart
@@ -11,38 +11,66 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(
           title: const Text("Title of App"),
         ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-              children:  [
-                SvgPicture.asset("assets/img/fluticon.svg",
+            children: [
+              SvgPicture.asset(
+                "assets/img/fluticon.svg",
                 // color: Colors.blue
-                ),
-                Image.asset('assets/img/pp.png',
-                height: 200 ,
-                width: 200,),
-                const Text(
-                  "Element for body",
-                  style: TextStyle(
+              ),
+              // Image.asset('assets/img/pp.png',
+              // height: 200 ,
+              // width: 200,),
+              const Text(
+                "Element for body",
+                style: TextStyle(
                     fontSize: 42,
                     fontFamily: 'Poppins',
-                    fontWeight: FontWeight.bold
-                  ),
-                ),
-                const Text("Second element for body with too many text for app",
+                    fontWeight: FontWeight.bold),
+              ),
+              const Text(
+                "Second element for body with too many text for app",
                 style: TextStyle(
                   fontSize: 24,
                 ),
                 textAlign: TextAlign.center,
-                )
-              ],
-            ),
-        )
-      ),
-    );
+              ),
+              const Padding(padding: EdgeInsets.only(top: 25)),
+              ElevatedButton.icon(
+                  style: const ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll(Colors.green),
+                    padding: MaterialStatePropertyAll(
+                        EdgeInsets.only(left: 20, right: 20)),
+                  ),
+                  onPressed: () => print("ElevatedButton Pressed: ok !"),
+                  label: const Text(
+                    "Button",
+                    style: TextStyle(
+                      fontFamily: "Poppins",
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  icon: const Icon(Icons.check),
+                  ),
+            ],
+          ),
+        ));
   }
 }
